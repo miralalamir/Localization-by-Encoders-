aa9767519048f7e11d5e432de7982d59628af0f3
miralalamir <miralalamir20@gmail.com>
2026-06-16 21:58:14 +0300

    Add more coditions and equations

--- Localization.py
+++ Localization.py
@@ -11,6 +11,7 @@ rpm = []
 distance = []
 d =0
 
+
 time = float(input(f"Time (seconds): "))
 
 for i in range(4):
@@ -21,8 +22,6 @@ for i in range(4):
     d += distance[i]
 
 d = d / 4
-x = x + d * (math.cos(30))
-y = y + d * (math.sin(30))
 
 
 for i in range(4):
@@ -30,13 +29,39 @@ for i in range(4):
     print(f"Distance {i+1}: {distance[i]}")
 
 print(d)
-distance[0] = 22*rpm[0]
 
+
+# 0 = r
+# 1 = l
+# 2 = r
+# 3 = l
 
 if rpm[0] == rpm[1]:
     print("straight")
-elif rpm[0] <= 0 and rpm[1] > 0:
+elif rpm[0] == 0 and rpm[1] > 0:
     print("right")
-elif rpm[0] > 0 and rpm[1] <= 0:
+    rotation = (rpm[1]*360) / 2*(math.pi*29.5)
+elif rpm[0] == 0 and rpm[1] < 0:
     print("left")
+    rotation = (rpm[0]*360) / 2*(math.pi*29.5)
+elif rpm[0] > 0 and rpm[1] < 0:
+    print("left")
+    roation = 29.5/2 #29.5 is the length between the wheels
+    # rotation = (rpm[1] * 29.5) / (rpm[0] - rpm[1])
+elif rpm[0] > 0 and rpm[1] > 0:
+    print("left")
+    roation = 29.5/2
+    # rotation = (rpm[0] * 29.5) / (rpm[1] - rpm[0])
+elif rpm[0] > rpm [1] or rpm[0] < rpm[1]:
+    #turn right
+    vr = (distance[0] + distance[2]) / (2 *time)
+    vl = (distance[1] + distance[3]) / (2 *time)
+    rotation = -(29.5/2) * ((vr + vl) / (vr - vl)) # it will be the same to get the right as positive and left as negative
+    
 
+
+print(rotation)
+x = x + d * (math.cos(rotation))
+y = y + d * (math.sin(rotation))
+
+print(x,y)
